1f068acd3f275b501a34ba69ba970a0d983df4f3
Ilona Zelinskaya <ilona.zelka@gmail.com>
2022-01-05 19:26:52 +0300

    Update CountryController.kt

--- src/main/kotlin/com/university/jpapostgresql/controller/CountryController.kt
+++ src/main/kotlin/com/university/jpapostgresql/controller/CountryController.kt
@@ -17,6 +17,24 @@ class CountryController {
     @Autowired
     lateinit var repository: CountryRepository
 
+    @RequestMapping("/countries")
+    fun findAll(): ResponseEntity<List<Country>> {
+        val countries = repository.findAll()
+        if (countries.isEmpty()) {
+            return ResponseEntity<List<Country>>(HttpStatus.NO_CONTENT)
+        }
+        return ResponseEntity<List<Country>>(countries, HttpStatus.OK)
+    }
+
+    @RequestMapping("/countries/{id}")
+    fun findById(@PathVariable id: Long): ResponseEntity<Country> {
+        val country = repository.findById(id)
+        if (country.isPresent) {
+            return ResponseEntity<Country>(country.get(), HttpStatus.OK)
+        }
+        return ResponseEntity<Country>(HttpStatus.NOT_FOUND)
+    }
+
     @PostMapping("/countries")
     fun addCountry(@RequestBody country: Country, uri: UriComponentsBuilder): ResponseEntity<Country> {
         val persistedCountry = repository.save(country)
@@ -26,6 +44,16 @@ class CountryController {
         val headers = HttpHeaders()
         headers.setLocation(uri.path("/country/{id}").buildAndExpand(country.id).toUri());
         return ResponseEntity(headers, HttpStatus.CREATED)
+    }
+
+    @PutMapping("/countries/{id}")
+    fun updateCountryById(@PathVariable("id") id: Long, @RequestBody country: Country): ResponseEntity<Country> {
+        return repository.findById(id).map { countryDetails ->
+            val updatedCountry: Country = countryDetails.copy(
+                country = country.country
+            )
+            ResponseEntity(repository.save(updatedCountry), HttpStatus.OK)
+        }.orElse(ResponseEntity<Country>(HttpStatus.INTERNAL_SERVER_ERROR))
     }
 
     @DeleteMapping("/countries/{id}")
