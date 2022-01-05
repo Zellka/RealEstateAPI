e01f8709b51b02dac09e201420a146633ee4a824
Ilona Zelinskaya <ilona.zelka@gmail.com>
2022-01-06 01:14:31 +0300

    Update CustomerRepository.kt

--- src/main/kotlin/com/university/jpapostgresql/repository/CustomerRepository.kt
+++ src/main/kotlin/com/university/jpapostgresql/repository/CustomerRepository.kt
@@ -2,8 +2,13 @@ package com.university.jpapostgresql.repository
 
 import com.university.jpapostgresql.model.Customer
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface CustomerRepository : JpaRepository<Customer, Long> {
+
+    @Query("select c from Customer c where c.phone = :phone")
+    fun findByPhone(phone: String): Optional<Customer>
 }
